e9e0ef7300f5e01b761f5348e23587c76be4af9b
Murilo Mattos <142946973+BlessedByJah@users.noreply.github.com>
2026-09-02 20:58:51 -0300

    Lista POO

---
+++ Quarto Semestre/POO/registrosEx.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+class REGISTRO
+{
+    private:
+}
+
+int main()
+{
+  REGISTRO registro;
+
+  registro.Cadastro();
+  registro.Consulta();
+  registro.ListarColaborador();
+  registro.DeletarColaborador();
+  registro.SalvarAqruivo();
+  registro.LerArquivo();  
+  
+  print ("  ======================== ");
+  printf("  == In future we trust == ");
+  print ("  ======================== ");
+}
